5d3a003fdb1199b665c05141e9c26d05f969e3e5
Dominik <dotto@fredhutch.org>
2022-10-28 12:55:00 -0700

    rename to mellon in doc build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../mellon'))
+sys.path.insert(0, os.path.abspath('../..'))
 # -- Project information -----------------------------------------------------
 
 project = 'Mellon'
