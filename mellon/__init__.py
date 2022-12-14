f40933c3e1bdaf90f82a95e90eb2343f647f8c6f
Dominik <dotto@fredhutch.org>
2022-12-13 17:48:11 -0800

    expose less internals to user

--- mellon/__init__.py
+++ mellon/__init__.py
@@ -8,9 +8,8 @@ from jax.config import config as jaxconfig
 jaxconfig.update("jax_enable_x64", True)
 jaxconfig.update("jax_platform_name", "cpu")
 
-from .base_cov import Covariance, Add, Mul, Pow
-from .util import stabilize, mle, distance, DEFAULT_JITTER
-from .decomposition import DEFAULT_RANK, DEFAULT_METHOD
+from .base_cov import Covariance
+from .util import stabilize, mle, distance
 from .cov import Matern32, Matern52, ExpQuad, Exponential, RatQuad
 from .inference import (
     compute_transform,
@@ -19,8 +18,6 @@ from .inference import (
     minimize_lbfgsb,
     compute_log_density_x,
     compute_conditional_mean,
-    DEFAULT_N_ITER,
-    DEFAULT_INIT_LEARN_RATE,
 )
 from .parameters import (
     compute_landmarks,
@@ -32,7 +29,6 @@ from .parameters import (
     compute_cov_func,
     compute_L,
     compute_initial_value,
-    DEFAULT_N_LANDMARKS,
 )
 from .derivatives import (
     gradient,
@@ -42,5 +38,4 @@ from .derivatives import (
 from .model import (
     DensityEstimator,
     FunctionEstimator,
-    DEFAULT_COV_FUNC,
 )
