b5f9d4161644c0be57a507b28c4bd35038593e45
Dominik <dotto@fredhutch.org>
2023-03-24 19:25:01 -0700

    more tests

--- tests/base_cov.py
+++ tests/base_cov.py
@@ -2,10 +2,13 @@ import jax.numpy as jnp
 import mellon
 
 
-def test_Matern32():
+def test_Add():
     n = 2
     d = 2
-    cov = mellon.Matern32(1.2)
+    cov1 = mellon.Matern32(1.4)
+    cov2 = mellon.Exponential(3.4)
+
+    cov = cov1 + cov2
     assert (
         len(str(cov)) > 0
     ), "The covariance function should have a string representation."
@@ -17,10 +20,13 @@ def test_Matern32():
     ), "Covariance should be computed for each pair of samples."
 
 
-def test_Matern52():
+def test_Mul():
     n = 2
     d = 2
-    cov = mellon.Matern52(1.2)
+    cov1 = mellon.Matern32(1.4)
+    cov2 = mellon.Exponential(3.4)
+
+    cov = cov1 * cov2
     assert (
         len(str(cov)) > 0
     ), "The covariance function should have a string representation."
@@ -32,40 +38,12 @@ def test_Matern52():
     ), "Covariance should be computed for each pair of samples."
 
 
-def test_ExpQuad():
+def test_Pow():
     n = 2
     d = 2
-    cov = mellon.ExpQuad(1.2)
-    assert (
-        len(str(cov)) > 0
-    ), "The covariance function should have a string representation."
-    x = jnp.ones((n, d))
-    values = cov(x, 2 * x)
-    assert values.shape == (
-        d,
-        d,
-    ), "Covariance should be computed for each pair of samples."
+    cov1 = mellon.Matern32(1.4)
 
-
-def test_Exponential():
-    n = 2
-    d = 2
-    cov = mellon.Exponential(1.2)
-    assert (
-        len(str(cov)) > 0
-    ), "The covariance function should have a string representation."
-    x = jnp.ones((n, d))
-    values = cov(x, 2 * x)
-    assert values.shape == (
-        d,
-        d,
-    ), "Covariance should be computed for each pair of samples."
-
-
-def test_RatQuad():
-    n = 2
-    d = 2
-    cov = mellon.RatQuad(3, 1.2)
+    cov = cov1**3.2
     assert (
         len(str(cov)) > 0
     ), "The covariance function should have a string representation."
--- tests/model.py
+++ tests/model.py
@@ -18,8 +18,8 @@ def test_DensityEstimator():
     d_std = jnp.std(log_dens)
 
     def relative_err(dens):
-        diff = jnp.abs(log_dens - dens)
-        return jnp.max(diff) / d_std
+        diff = jnp.std(log_dens - dens)
+        return diff / d_std
 
     pred_log_dens = est.predict(X)
     assert relative_err(pred_log_dens) < 1e-5, (
@@ -27,31 +27,61 @@ def test_DensityEstimator():
         "the training samples."
     )
 
+    grads = est.gradient(X)
+    assert (
+        grads.shape == X.shape
+    ), "The gradient should have the same shape as the input."
+
+    hess = est.hessian(X)
+    assert hess.shape == (n, d, d), "The hessian should have the correct shape."
+
+    result = est.hessian_log_determinant(X)
+    assert (
+        len(result) == 2
+    ), "hessian_log_determinan should return signes and lg-values."
+    sng, ld = result
+    assert sng.shape == (n,), "There should be one sign for each hessian determinan."
+    assert ld.shape == (n,), "There should be one value for each hessian determinan."
+
     assert len(str(est)) > 0, "The model should have a string representation."
 
     est_full = mellon.DensityEstimator(rank=1.0, method="percent", n_landmarks=n)
-    full_log_dens = est_full.fit_predict(X)
+    est_full.fit(X)
+    full_log_dens = est_full.predict(X)
     assert (
         relative_err(full_log_dens) < 1e-1
     ), "The default approximation should be close to the full rank result."
 
     est = mellon.DensityEstimator(rank=1.0, method="percent", n_landmarks=10)
-    dens_appr = est.fit_predict(X)
+    est.fit(X)
+    dens_appr = est.predict(X)
     assert (
         relative_err(dens_appr) < 2e-1
     ), "The low landmarks approximation should be close to the default."
 
     est = mellon.DensityEstimator(rank=0.99, method="percent", n_landmarks=80)
-    dens_appr = est.fit_predict(X)
+    est.fit(X)
+    dens_appr = est.predict(X)
     assert (
         relative_err(dens_appr) < 2e-1
     ), "The low landmarks + Nystrom approximation should be close to the default."
 
     est = mellon.DensityEstimator(rank=50, n_landmarks=80)
-    dens_appr = est.fit_predict(X)
+    est.fit(X)
+    dens_appr = est.predict(X)
     assert (
         relative_err(dens_appr) < 2e-1
     ), "The low landmarks + Nystrom approximation should be close to the default."
+
+    est = mellon.DensityEstimator()
+    d1_pred = est.fit_predict(X[:, 0])
+    assert d1_pred.shape == (n,), "There should be one result per sample."
+
+    est = mellon.DensityEstimator(rank=1.0, method="percent", n_landmarks=0)
+    d1_pred_full = est.fit_predict(X[:, 0])
+    assert (
+        jnp.std(d1_pred - d1_pred_full) < 1e-2
+    ), "The scalar state function estimations be consistent under approximation."
 
 
 def test_FunctionEstimator():
@@ -73,7 +103,7 @@ def test_FunctionEstimator():
     err = jnp.std(y - pred)
     assert err < 1e-5, "The prediction should be close to the intput value."
 
-    est_full = mellon.FunctionEstimator(rank=1.0, method="percent", n_landmarks=n)
+    est_full = mellon.FunctionEstimator(rank=1.0, method="percent", n_landmarks=0)
     full_pred = est_full.fit_predict(X, y)
     err = jnp.max(jnp.abs(full_pred - pred))
     assert (
@@ -98,3 +128,13 @@ def test_FunctionEstimator():
     assert (
         err < 1e-1
     ), "The low landmarks + Nystrom approximation should be close to the default."
+
+    est = mellon.FunctionEstimator()
+    d1_pred = est.fit_predict(X[:, 0], y)
+    assert d1_pred.shape == (n,), "There should be one result per sample."
+
+    est = mellon.FunctionEstimator(rank=1.0, method="percent", n_landmarks=0)
+    d1_pred_full = est.fit_predict(X[:, 0], y)
+    assert (
+        jnp.std(d1_pred - d1_pred_full) < 1e-5
+    ), "The scalar state function estimations be consistent under approximation."
