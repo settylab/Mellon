d82d8fd2c477b4c3bd1f0892b300f8539ede5db2
Dominik <dotto@fredhutch.org>
2023-05-17 16:21:45 -0700

    sliced L

--- mellon/decomposition.py
+++ mellon/decomposition.py
@@ -1,6 +1,7 @@
 from jax.numpy import cumsum, searchsorted, count_nonzero, sqrt, isnan, any
 from jax.numpy.linalg import eigh, cholesky, qr
 from jax.scipy.linalg import solve_triangular
+from jax import vmap
 from .util import stabilize, DEFAULT_JITTER, Log
 
 
@@ -178,7 +179,6 @@ def _standard_low_rank(x, cov_func, xu, jitter=DEFAULT_JITTER):
     :rtype: array-like
     """
     W = stabilize(cov_func(xu, xu), jitter)
-    C = cov_func(x, xu)
     U = cholesky(W)
     if any(isnan(U)):
         message = (
@@ -187,7 +187,13 @@ def _standard_low_rank(x, cov_func, xu, jitter=DEFAULT_JITTER):
         )
         logger.error(message)
         raise ValueError(message)
-    L = solve_triangular(U, C.T, lower=True).T
+
+    def L_slice(X):
+        C = cov_func(X[None, :], xu)
+        return solve_triangular(U, C.T, lower=True)[:, 0]
+
+    L = vmap(L_slice)(x)
+
     return L
 
 
