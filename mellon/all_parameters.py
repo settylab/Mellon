472ffe06d8dbc0b078f58da86f1ad5e5acc7b2a1
Dominik <dotto@fredhutch.org>
2023-06-16 11:59:00 -0700

    update docstring of derivatives submdolue

--- mellon/all_parameters.py
+++ mellon/all_parameters.py
@@ -35,14 +35,14 @@ Why this file exists:
 =====================
 In the Mellon library, the compute_ls_time module deserves its own file due to its
 complexity and size. However, it's also a part of the parameters submodule and it
-imports the DensityEstimator, which leads to a potential circular import issue if 
+imports the DensityEstimator, which leads to a potential circular import issue if
 we tried to import from the paremeters submodule into the DensityEstimator module.
 
-This file, all_parameters.py, is a workaround to avoid this circular import problem. 
-It gathers all the necessary functions from the parameters submodule, including 
-compute_ls_time, and makes them available for import as a package in the __init__.py 
+This file, all_parameters.py, is a workaround to avoid this circular import problem.
+It gathers all the necessary functions from the parameters submodule, including
+compute_ls_time, and makes them available for import as a package in the __init__.py
 file.
 
-Hence, when the DensityEstimator module needs to use the functions defined in 
+Hence, when the DensityEstimator module needs to use the functions defined in
 parameters.py, it can does not import itselfe.
 """
--- mellon/derivatives.py
+++ mellon/derivatives.py
@@ -1,5 +1,5 @@
 import jax
-from jax.numpy import isscalar
+from jax.numpy import isscalar, atleast_2d
 
 from .validation import _validate_1d, _validate_float
 
@@ -46,73 +46,92 @@ def derivative(function, x, jit=True):
 
 
 def gradient(function, x, jit=True):
-    R"""
-    Conputes the gradient of the function for each line in x.
-
-    :param function: A function returning one value per
-        line in x. function(x).shape == (x.shape[0], )
-    :type function: function
-    :param x: Data points.
-    :type x: array-like
-    :param jit: Use jax just in time compilation. Defaults to True.
-    :type jit: bool
-    :return: gradiants - The gradient of function at each point in x.
-        gradients.shape == x.shape
-    :rtype: array-like
     """
+    Computes the gradient of a function for each line in `x`.
 
+    Parameters
+    ----------
+    function : callable
+        A function that takes a scalar input and outputs a scalar.
+        The function must have the signature function(x: scalar) -> scalar.
+    x : array-like
+        Data points at which the gradient is to be computed.
+    jit : bool, optional
+        If True, use JAX's just-in-time (JIT) compilation to speed up the computation. Defaults to True.
+
+    Returns
+    -------
+    array-like
+        The gradient of the function at each point in `x`.
+        The shape of the output array is the same as `x`.
+    """
 
     def get_grad(x):
         return jax.jacrev(function)(x[None, :])
 
     if jit:
         get_grad = jax.jit(get_grad)
-    return jax.vmap(get_grad, in_axes=(0,))(x)[:, 0, 0, :]
+    return jax.vmap(get_grad, in_axes=(0,))(x).reshape(x.shape)
 
 
 def hessian(function, x, jit=True):
-    R"""
-    Conputes the hessian of the function for each line in x.
-
-    :param function: A function returning one value per
-        line in x. function(x).shape == (x.shape[0], )
-    :type function: function
-    :param x: Data points.
-    :type x: array-like
-    :param jit: Use jax just in time compilation. Defaults to True.
-    :type jit: bool
-    :return: hessians - The hessian matrix of function at each point in x.
-        hessians.shape == X.shape + X.shape[1:]
-    :rtype: array-like
     """
+    Computes the gradient of a function for each line in `x`.
+
+    Parameters
+    ----------
+    function : callable
+        A function that takes a scalar input and outputs a scalar.
+        The function must have the signature function(x: scalar) -> scalar.
+    x : array-like
+        Data points at which the gradient is to be computed.
+    jit : bool, optional
+        If True, use JAX's just-in-time (JIT) compilation to speed up the computation. Defaults to True.
+
+    Returns
+    -------
+    array-like
+        The gradient of the function at each point in `x`.
+        The shape of the output array is the same as `x`.
+    """
+    x = atleast_2d(x)
 
     def get_hess(x):
         return jax.jacfwd(jax.jacrev(function))(x[None, :])
 
     if jit:
         get_hess = jax.jit(get_hess)
-    return jax.vmap(get_hess, in_axes=(0,))(x)[:, 0, 0, :, 0, :]
+    out_shape = x.shape + x.shape[1:]
+    return jax.vmap(get_hess, in_axes=(0,))(x).reshape(out_shape)
 
 
 def hessian_log_determinant(function, x, jit=True):
-    R"""
-    Conputes the logarirhm of the determinat of the function for each line in x.
-
-    :param function: A function returning one value per
-        line in x. function(x).shape == (x.shape[0], )
-    :type function: function
-    :param x: Data points.
-    :type x: array-like
-    :param jit: Use jax just in time compilation. Defaults to True.
-    :type jit: bool
-    :return: signs, log_determinants - The sign of the determinant
-        at each point x and the logarithm of its absolute value.
-        signs.shape == log_determinants.shape == x.shape[0]
-    :rtype: array-like, array-like
     """
+    Computes the logarithm of the determinant of the Hessian for each line in `x`.
+
+    Parameters
+    ----------
+    function : callable
+        A function that takes a scalar input and outputs a scalar.
+        The function must have the signature function(x: scalar) -> scalar.
+    x : array-like
+        Data points at which the log determinant of the Hessian is to be computed.
+    jit : bool, optional
+        If True, use JAX's just-in-time (JIT) compilation to speed up the computation. Defaults to True.
+
+    Returns
+    -------
+    array-like, array-like
+        The sign of the determinant at each point in `x` and the logarithm of its absolute value.
+        `signs.shape == log_determinants.shape == x.shape[0]`.
+    """
+    x = atleast_2d(x)
+
+    d = x.shape[1]
+    hess_shape = (d, d)
 
     def get_log_det(x):
-        hess = jax.jacfwd(jax.jacrev(function))(x[None, :])[0, 0, :, 0, :]
+        hess = jax.jacfwd(jax.jacrev(function))(x[None, :]).reshape(hess_shape)
         sign, log_det = jax.numpy.linalg.slogdet(hess)
         return sign, log_det
 
