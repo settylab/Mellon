3ea5ac69ba6fe85950971a3b002c6d3fdf4a34c5
Dominik <dotto@fredhutch.org>
2024-02-15 18:39:51 -0800

    add linear kernel

--- mellon/cov.py
+++ mellon/cov.py
@@ -1,4 +1,4 @@
-from jax.numpy import sqrt, exp, square
+from jax.numpy import sqrt, exp, square, einsum, repeat
 from .base_cov import Covariance
 from .util import distance, select_active_dims, distance_grad
 
@@ -95,7 +95,6 @@ class Matern32(Covariance):
         return k_grad
 
 
-
 class Matern52(Covariance):
     R"""
     Implementation of the Matern-5/2 kernel function, a member of the Matern
@@ -152,10 +151,10 @@ class Matern52(Covariance):
         """
         x = select_active_dims(x, self.active_dims)
         y = select_active_dims(y, self.active_dims)
-        r = (sqrt(5.0) * distance(x, y) / self.ls)
+        r = sqrt(5.0) * distance(x, y) / self.ls
         similarity = (r + square(r) / 3 + 1) * exp(-r)
         return similarity
-    
+
     def k_grad(self, x):
         """
         Generate a function to compute the gradient of the Matern-5/2 kernel function.
@@ -187,7 +186,7 @@ class Matern52(Covariance):
             dist, grad = dist_grad(y)
             r = factor * dist[..., None]
             dr = factor * grad
-            similarity_grad = -1/3 * exp(-r) * r * (r + 1) * dr
+            similarity_grad = -1 / 3 * exp(-r) * r * (r + 1) * dr
             return similarity_grad
 
         return k_grad
@@ -248,7 +247,7 @@ class ExpQuad(Covariance):
         r = distance(x, y) / self.ls
         similarity = exp(-square(r) / 2)
         return similarity
-    
+
     def k_grad(self, x):
         """
         Generate a function to compute the gradient of the Exponentiated Quadratic kernel function.
@@ -279,7 +278,7 @@ class ExpQuad(Covariance):
             dist, grad = dist_grad(y)
             r = dist[..., None] / self.ls
             dr = grad / self.ls
-            similarity_grad = - r * dr * exp(-square(r) / 2)
+            similarity_grad = -r * dr * exp(-square(r) / 2)
             return similarity_grad
 
         return k_grad
@@ -371,7 +370,7 @@ class Exponential(Covariance):
             dist, grad = dist_grad(y)
             r = dist[..., None] / self.ls
             dr = grad / self.ls
-            similarity_grad = - 1/2 * dr * exp(-r / 2)
+            similarity_grad = -1 / 2 * dr * exp(-r / 2)
             return similarity_grad
 
         return k_grad
@@ -467,7 +466,98 @@ class RatQuad(Covariance):
             dist, grad = dist_grad(y)
             r = dist[..., None] / self.ls
             dr = grad / self.ls
-            similarity_grad = - r * dr * (square(r) / (2 * self.alpha) + 1)**(-self.alpha - 1)
+            similarity_grad = (
+                -r * dr * (square(r) / (2 * self.alpha) + 1) ** (-self.alpha - 1)
+            )
+            return similarity_grad
+
+        return k_grad
+
+
+class Linear(Covariance):
+    R"""
+    Implementation of the Linear kernel.
+
+    TheL inear kernel function is defined as:
+
+    .. math::
+
+        (1 + \frac{\sqrt{5}||x-y||}{l} + \frac{5||x-y||^2}{3l^2})
+        \cdot e^{-\frac{\sqrt{5}||x-y||}{l}}
+
+    where `x` and `y` are input vectors and `l` is the length-scale.
+
+    This class can be used as a function curry, meaning it can be called
+    like a function on two inputs `x` and `y`.
+
+    Parameters
+    ----------
+    ls : float, optional
+        The length-scale parameter, which controls the width of the kernel.
+        Larger values result in wider kernels, and smaller values in narrower kernels.
+        Default is 1.0.
+
+    active_dims : array-like, slice or scalar, optional
+        The indices of the active dimensions. If specified, the kernel function
+        will only be computed over these dimensions. Default is None, which means
+        all dimensions are active.
+    """
+
+    def __init__(self, ls=1.0, active_dims=None):
+        super().__init__()
+        self.ls = ls
+        self.active_dims = active_dims
+
+    def k(self, x, y):
+        """
+        Compute the Linear kernel between inputs `x` and `y`.
+
+        Parameters
+        ----------
+        x : array-like
+            First input array.
+        y : array-like
+            Second input array.
+
+        Returns
+        -------
+        similarity : float
+            The computed kernel function.
+        """
+        x = select_active_dims(x, self.active_dims)
+        y = select_active_dims(y, self.active_dims)
+
+        similarity = einsum("ij,kj->ik", x, y) / self.ls
+
+        return similarity
+
+    def k_grad(self, x):
+        """
+        Generate a function to compute the gradient of the Linear kernel function.
+
+        This method returns a callable that, when given an array `y`, computes the gradient
+        of the Linear kernel function with respect to `y`, considering `x` as the fixed
+        input. The computation is restricted to the active dimensions specified in the
+        covariance function instance.
+
+        Parameters
+        ----------
+        x : array-like
+            The fixed input array used as the first argument in the Linear kernel.
+            Its shape should be compatible with the active dimensions of the kernel.
+
+        Returns
+        -------
+        Callable
+            A function that takes an array `y` as input and returns the gradient of the
+            Linear kernel function with respect to `y`, evaluated at the pair `(x, y)`.
+            The gradient is computed only over the active dimensions.
+        """
+        x = select_active_dims(x, self.active_dims)
+
+        def k_grad(y):
+            y = select_active_dims(y, self.active_dims)
+            similarity_grad = repeat(x[:, None, :], y.shape[0], axis=1) / self.ls
             return similarity_grad
 
         return k_grad
