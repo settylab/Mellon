33f22381e2c047a3a1b91f57058c604c5fb988c6
Dominik <dotto@fredhutch.org>
2024-06-19 21:03:17 -0700

    bunmp version 1.4.3

--- mellon/__init__.py
+++ mellon/__init__.py
@@ -18,7 +18,7 @@ from . import _inference as inference
 from . import _conditional as conditional
 from . import _derivatives as derivatives
 
-__version__ = "1.4.2"
+__version__ = "1.4.3"
 
 __all__ = [
     "DensityEstimator",
